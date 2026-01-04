3b8df20139cc4564f3dc4e3779c69f93ddb62566
HonseGathGath <lassouedghaith15@gmail.com>
2026-01-04 14:53:07 +0000

    commit

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,27 +1,55 @@
 export interface AnnuiteInput {
-  capitalInitial: number;
-  tauxInteret: number;
-  periode: number;
-  type: "debut" | "fin";
+	capitalInitial: number;
+	tauxInteret: number;
+	periode: number;
+	type: "debut" | "fin";
 }
 
 export interface EscompteInput {
-  valeurNominale: number;
-  tauxEscompte: number;
-  jours: number;
-  joursBanque: number;
+	valeurNominale: number;
+	tauxEscompte: number;
+	jours: number;
+	joursBanque: number;
 }
 
 export interface EmpruntInput {
-  capitalEmprunte: number;
-  tauxInteret: number;
-  duree: number;
-  type: "annuites_constantes" | "amortissement_constants" | "in_fine";
+	capitalEmprunte: number;
+	tauxInteret: number;
+	duree: number;
+	type: "annuites_constantes" | "amortissement_constants" | "in_fine";
 }
 
-export interface AmortizationRow {
-  capitalRestant: number;
-  interet: number;
-  amortissement: number;
-  annuite: number;
+export interface EquivalenceInput {
+	valeur1: number;
+	jours1: number;
+	valeur2: number;
+	jours2: number;
+	taux: number;
+}
+
+// RESULT TYPES
+export interface AnnuiteResult {
+	valeurAcquise: number;
+	valeurActuelle: number;
+	annuiteConstante: number;
+}
+
+export interface EscompteResult {
+	escompteCommercial: number;
+	valeurActuelleCommerciale: number;
+	escompteRationnel: number;
+	valeurActuelleRationnelle: number;
+	diviseur: number;
+}
+
+export interface EmpruntResult {
+	annuite: number;
+	amortissement: number;
+	interet: number;
+	capitalRestant: number;
+}
+
+export interface EquivalenceResult {
+	dateEquivalence: number;
+	valeurActuelle: number;
 }
